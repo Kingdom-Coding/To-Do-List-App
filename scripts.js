33a4a894339a7c1240242b599244a31abc0a7b07
Kingdom-Coding <jonathanv2283219@gmail.com>
2024-12-13 19:22:42 -0500

    updated the js file to finish the functionality of the menu btns
    *added notes to the javaScript to make it more readable
    *added functionality to the first two menu buttons in the menu container
    *the addItem function is complete
    *the crossout function is complete
    *the deleteItem function is complete

--- scripts.js
+++ scripts.js
@@ -12,16 +12,18 @@ const toDoList = (() => {
   const crossOutBtn = $(".create-checklist");
   const deleteItemBtn = $(".delete-item");
 
+  //function to call all the functions that handle individual functionality
   function functionality() {
     menuContainer.hide();
     menu();
     addItem();
     addItemValue();
     crossOut();
+    deleteItem();
   }
 
   function menu() {
-    //show the menu container when clicking on the button
+    //an event listener on the ol to dynamically open every menu container when the menu buttons for each item are clicked
     ol.on("click", li, (e) => {
       const target = $(e.target);
       if (target.hasClass("menu-btn")) {
@@ -37,7 +39,7 @@ const toDoList = (() => {
       }
     });
 
-    //close menu-container when click outside of it or on another button
+    //an event listner to close the menu-container when clicking outside of it or on another button
     $(document).on("click", (e) => {
       const target = $(e.target);
 
@@ -62,12 +64,9 @@ const toDoList = (() => {
     return menu;
   }
 
-  function getItemValue(item) {
-    return item.val();
-  }
-
+  //function that handles the input functionality, allowing users to add their items withe the enter button
   function addItemValue() {
-    //close the focused item if it has no value
+    //an event listener that checks to see if the enter key is pressed when the item is focused
     $("ol").on("keydown", $("input"), (e) => {
       if ($("input").is($(e.target))) {
         const target = $(e.target);
@@ -82,88 +81,139 @@ const toDoList = (() => {
         }
       }
     });
-    /* let previousTarget = null;
-    $(document).on("click", (e) => {
-
-       if (
-        $(previousTarget).is("input") &&
-        !$(previousTarget).parent().hasClass("firstItem") &&
-        noInput === ""
-      ) {
-        if ($(previousTarget).is(".firstItem")) {
-          !$(".firstItem").hide();
-        } else {
-          $(previousTarget).parent("li").hide();
-        }
-      }
-      previousTarget = e.target; 
-    }); */
   }
 
+  //a function to make the add item button functional
   function addItem() {
-    //adds an item to the list
+    //an event listener that dynamically updates the ol by adding an item and their respective elements to the ol when the add item button is pressed
     ol.on("click", (e) => {
       const target = $(e.target);
+      //if statement to handle the actions when the add item button is pressed
       if (target.is($(".add-item"))) {
+        //new item elements
         const currentMenuContainer = $(".menu-container");
-        const newLi = $('<li class="list-item items"></li>');
+        const newLi = $('<li class="list-item new-li items"></li>');
         const newMenuBtn = $('<button class="menu-btn btn">*</button>');
         const newInput = $(
-          '<input class="item" type="text" placeholder="create item" />'
+          '<input class="new-input item" type="text" placeholder="create item" />'
         );
         const newMenuContainer = $("<div class='menu-container'></div>");
         const newAddItemBtn = $(
           '<button class="add-item btn">Add Item</button>'
         );
         const crossOutBtn = $(
-          "<button class='create-checklist btn'>Make Checklist</button>"
+          "<button class='cross-out' btn'>Cross Out</button>"
         );
         const newDeleteItemBtn = $(
           "<button class='delete-item btn'>Delete Item</button>"
         );
 
+        //making sure the menu's are hidden when adding another menu container
         currentMenuContainer.hide();
-
         newMenuContainer.hide();
 
+        //appending the elements
         newMenuContainer.append(newAddItemBtn, crossOutBtn, newDeleteItemBtn);
         newLi.append(newMenuContainer, newMenuBtn, newInput);
-        ol.append(newLi);
+        $('.item-list').append(newLi);
+
+        //making sure the newly added input from the added item is focused when we add the item to the list
         newInput.focus();
 
-        const newItemValue = getItemValue(newInput);
-
-        if (newItemValue === "") {
-          $(document).on("click", (e) => {
-            if (
-              !$(e.tartet).is(".item") &&
-              !$(e.target).closest(".menu-container").length
-            ) {
-              console.log($(e.target));
-              $(newInput).parent("li").hide();
-              console.log("hid");
+        //an event listener to delete the newly added li if the user chooses to add no value to it when it's created
+        $(document).on("click", (e) => {
+          const target = $(e.target);
+          // if statement to narrow down the selection of the users target when clicking the mouse by making sure it's not the menu container, it's respective elements,or the newly added input itself
+          if (
+            !target.is($(".item")) &&
+            !target.closest(".menu-container").length
+          ) {
+          //if statement to make sure that if a newly added item is added to the ol with no value and the use chooses to click outside of it, then the item gets deleted
+            if (ol.children(".new-li") && $(newInput).val() === "") {
+              newInput.parent("li").remove();
+              console.log("hello");
+              $(document).off();
             }
-          });
-        }
+          }
+        });
       }
     });
   }
-
+  //a function to make the cross out button functional 
   function crossOut() {
+    //an event listener to dynamically update each individual button if the cross out button is checked to cross out the parent li's input
+    const errorMessageVisible = $('.error-message').css('visibility', true);
     ol.on("click", (e) => {
-      const target = $(e.target);
-      if (target.is($(".cross-out"))) {
-        console.log("hi");
+      const inputElement = $(e.target).parents('li').children('.item');
+      //if statement to cross out the item when the cross out button is clicked
+      if ($(e.target).is($(".cross-out")) && !inputElement.parent('.first-item')) {
+        inputElement.addClass("strike");
+        inputElement.attr("disabled", true);
+        $('.menu-container').hide();
       }
+      //if statement to move the crossout item to the done section
+      if (inputElement.hasClass('strike') && !inputElement.parent('.first-item')) {
+        $('.finished-items-list-container').append(inputElement.parent('li'));
+      }
+      //if statement to give an error if first item has no value
+      if ($(e.target).is('.cross-out') && inputElement.parent('.first-item') && inputElement.val() === ''){
+        $('.error-message').css('visibility', 'visible');
+        //adds a click event listener so that the error leaves when you click on the first item's input
+        $('.first-item').on('click', () => {
+        $('.error-message').css('visibility', 'hidden');
+     })
+      }
+    });
+    
+   
+  }
+
+  //a function to make the delete button functional
+  function deleteItem() {
+    ol.on("click", (e) => {
+        if (
+          $(e.target).is(".delete-item") &&
+          !$(e.target).parents().is(".first-item")
+        ) {
+          $(e.target).parents("li").remove();
+          console.log($(e.target).parent());
+        }
+      }); 
+    //event listener for handling the first item when clicking the delete button
+    $(".delete-item").on("click", (e) => {
+      //global variables for the event
+      const inputElement = $(e.target).parents("li").children(".item");
+      const errorMessage = $('.error-message');
+      //if statement to delete the value of the first item's input
+      if (
+        $(e.target).is(".delete-item") &&
+        $(e.target).parents().is(".first-item")
+      ) {
+        inputElement.val("");
+      }
+      //if statement to create an error message if the delete button is clicked when the first item has no value
+      if ($(e.target).is(".delete-item") && inputElement.val() === "") {
+        $(".delete-item").parent().hide();
+        errorMessage.css('visibility', 'visible');
+        //adds a listener to the first item to remove the error when the first item's input gets clicked
+        $(".first-input").on("click", () => {
+          $(".error-message").css('visibility', 'hidden');
+        });
+      }
+      //if statement to delete the value of the first item if the delete button is clicked
+      if ($(e.target).is('.delete-item') && inputElement.val() === true) {
+        inputElement.val() = '';
+      }
+      //event listener to delete added items to the list
+     
     });
   }
 
-  function deleteItem() {}
-
+  //returns the functionality function so it's accessible outside of the IIFE;
   return {
     functionality: functionality,
-    menu: menu,
   };
 })();
 
+//calls the functionality function from the toDoList IIFE
 toDoList.functionality();
